d3f0b26d39dd46674afb38ab28005f3aadfcbe87
SteveWonderbelt <schilds@email.sc.edu>
2019-12-08 21:10:54 -0500

    new

--- LearnYouHaskell/baby.hs
+++ LearnYouHaskell/baby.hs
@@ -1,3 +1,4 @@
+import qualified Data.Map as Map
 lucky :: (Integral a)=> a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "sorry you're out of luck, pal"
@@ -250,3 +251,85 @@ type Phonebook  = [(String, String)]
 type PhoneNumber = String
 type Name = String
 type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook
+
+type AssocList k v = [(k,v)]
+
+data Either' a b = Left' a | Right' b deriving(Eq, Ord, Read, Show)
+
+data LockerState= Taken | Free deriving (Show, Eq)
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+{-data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord) -}
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty       .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+data Tree a  = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a 
+singleton x = Node x EmptyTree EmptyTree 
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a 
+treeInsert x EmptyTree = singleton x 
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert x left) right
+    | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
+
+data TrafficLight  = Red | Yellow | Green
+instance Eq TrafficLight where 
+    Red == Red =True
+    Green == Green = True
+    Yellow == Yellow = True
+    _==_ = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow Light"
+    show Green = "Green Light"
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+     yesno Red = False
+     yesno _ = True
+
+
+
+data Barry t k p = Barry {yabba :: p , dabba :: t k}
